a1986786dcbe708a36174c91842d7ca4623a4248
xlHenshin <andreslayer117@gmail.com>
2020-12-10 18:28:58 -0500

    Imagenes actualizadas

--- src/model/Revy.java
+++ src/model/Revy.java
@@ -1,5 +1,15 @@
 package model;
 
+import processing.core.PApplet;
+import processing.core.PImage;
+
 public class Revy {
 
+	private PApplet app;
+	private PImage revy;
+	
+	public Revy(PApplet app) {
+		
+		revy= app.loadImage("../Resources/revy1.png");
+	}
 }
